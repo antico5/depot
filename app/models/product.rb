e46a37862859e437a18b2526d2169dc1bdcb322f
Armando Andini <armando.andini@hotmail.com>
2016-07-13 18:54:08 -0300

    Add relationship between cart, product and line_items.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+  has_many :line_items
+  before_destroy :ensure_not_referenced
+
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true
@@ -7,6 +10,15 @@ class Product < ApplicationRecord
 
   def self.latest
     order(:updated_at).last
+  end
+
+  private
+
+  def ensure_not_referenced
+    unless line_items.empty?
+      errors.add :base, 'line items not empty'
+      throw :abort
+    end
   end
 end
 
