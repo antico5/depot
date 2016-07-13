a9df5dccc134fd39723ca01ef69475433072de99
Armando Andini <armando.andini@hotmail.com>
2016-07-13 18:55:56 -0300

    Test "shouldn't destroy product if it has line items referencing it".

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -46,4 +46,11 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to products_url
   end
+
+  test "shouldn't destroy product if it has line items referencing it" do
+    assert_difference('Product.count', 0) do
+      delete product_url(products(:two))
+      assert_redirected_to products_url
+    end
+  end
 end
