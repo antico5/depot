330e79a3fc36858424ef451ce0bf256c3a2cedad
Armando Andini <armando.andini@hotmail.com>
2016-07-18 10:13:40 -0300

    Cart#add_product method and tests.

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @cart = Cart.new
+  end
+
+  test "cart has one line item per product" do
+    @cart.add_product(products(:one)).save
+    assert_equal 1, @cart.line_items.count
+  end
+
+  test "cart adds up to line item quantity when adding the same product" do
+    3.times do
+      @cart.add_product(products(:two)).save
+    end
+    assert_equal 1, @cart.line_items.count
+    assert_equal 3, @cart.line_items.last.quantity
+  end
 end
