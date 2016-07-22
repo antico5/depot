bc980b31ee7d9850fcf0399d18be5516ca5e74ba
Armando Andini <armando.andini@hotmail.com>
2016-07-22 11:00:12 -0300

    Add validations to order model and update fixtures.

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,8 @@ class Order < ApplicationRecord
     "Credit Card" => 1,
     "Purchase Order" => 2
   }
+
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: Order.pay_types.keys
+  validates_format_of :email, with: /@/
 end
