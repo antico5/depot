568d8af9801d48a6323e6b6fcf57627b0ca5020b
Armando Andini <armando.andini@hotmail.com>
2016-07-23 10:42:26 -0300

    Add who_bought action and Atom feed.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :orders
   resources :line_items
   resources :carts
-  resources :products
+  resources :products do
+    get 'who_bought', on: :member
+  end
   root "store#index", as: 'store_index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
