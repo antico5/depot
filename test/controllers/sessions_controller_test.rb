815de908cdaebbe0b076ee4282480d2ef3c2537f
Armando Andini <armando.andini@hotmail.com>
2016-07-25 11:43:33 -0300

    SessionController tests.

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,19 +1,30 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get sessions_new_url
+  setup do
+    @user = users :one
+  end
+
+  test "should get login form" do
+    get login_url
     assert_response :success
   end
 
-  test "should get create" do
-    get sessions_create_url
-    assert_response :success
+  test "should log user in with correct name and password" do
+    post_on_login
+    assert_equal session[:user_id], @user.id
+    assert_redirected_to admin_index_url
   end
 
-  test "should get destroy" do
-    get sessions_destroy_url
-    assert_response :success
+  test "should log user out" do
+    post_on_login
+    delete logout_url
+    assert_nil session[:user_id]
+    assert_redirected_to store_index_url
+  end
+
+  def post_on_login
+    post login_url, params: { name: @user.name, password: 'secret' }
   end
 
 end
