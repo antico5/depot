e35f3e1689e5a21d716df1c48a8b9c0b4a735cf1
Armando Andini <armando.andini@hotmail.com>
2016-07-26 09:10:40 -0300

    Store chosen locale on the session.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authorize!
-  before_action :set_i18n_locale_from_params
+  before_action :set_i18n_locale
 
   protected
 
@@ -12,14 +12,17 @@ class ApplicationController < ActionController::Base
       current_user || redirect_to(login_url, notice: 'Please log in')
     end
 
-    def set_i18n_locale_from_params
-      return unless params[:locale]
+    def set_i18n_locale
+      locale = params[:locale]
+      session[:locale] = locale if locale
 
-      if I18n.available_locales.map(&:to_s).include? params[:locale]
-        I18n.locale = params[:locale]
-      else
-        flash.now[:notice] = "There is no translation associated for #{ params[:locale] }."
-        logger.error flash.now[:notice]
+      if session[:locale]
+        if I18n.available_locales.map(&:to_s).include? session[:locale]
+          I18n.locale = session[:locale]
+        else
+          flash.now[:notice] = "There is no translation associated for #{ session[:locale] }."
+          logger.error flash.now[:notice]
+        end
       end
     end
 end
