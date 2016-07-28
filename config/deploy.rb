0d920b08f9033aa84fe484fbe5bdd0b5e6c846c8
Armando Andini <armando.andini@hotmail.com>
2016-07-28 15:46:33 -0300

    Remove 'bin' folder from linked directories, to make rails commands work on the server.

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,8 @@ set :branch, :master
 set :deploy_to, '/home/deploy/depot'
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
-set :linked_dirs, %w{ bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads }
+set :linked_dirs, %w{ log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads }
+set :bundle_binstubs, nil
 set :keep_releases, 5
 
 # RVM config
